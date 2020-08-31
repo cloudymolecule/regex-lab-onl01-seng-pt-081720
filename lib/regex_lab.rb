b2704509569b012a72862a158967da9021e2ae70
Dario Carlino <baires82@gmail.com>
2020-08-31 15:23:16 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,10 +16,10 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match?(/[A-Z].*[.,:;]$/)
-    true
-  elsif text.match?(/[a-z].*[.,:;]$/)
+  if text.match(/[A-Z].*[.,:;]$/) == nil
     false
+  else
+    true
   end
 end
 
