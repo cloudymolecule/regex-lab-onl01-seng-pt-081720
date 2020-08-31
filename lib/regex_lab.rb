cec8e939f40533ff87880c20636352c2f02c8490
Dario Carlino <baires82@gmail.com>
2020-08-31 15:32:42 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,12 +16,12 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.scan(/[A-Z].*[.,:;]$/)
+  if text.scan(/[a-z].*[.,:;]$/)
+    false
+  elsif text.match(/[A-Z].*[.,:;]$/)
     true
-  elsif text.match(/[a-z].*[.,:;]$/)
-    nil
   else
-    nil
+    false
   end
 
 end
