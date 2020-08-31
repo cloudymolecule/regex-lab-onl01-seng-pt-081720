b614f55b416001a2f017be34ab7e857891390541
Dario Carlino <baires82@gmail.com>
2020-08-31 15:25:40 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,10 +16,13 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/[A-Z].*[.,:;]$/)
-  text.match(/[a-z].*[.,:;]$/)
+  if text.match(/[A-Z].*[.,:;]$/)
+    true
 
-  
+  elsif text.match(/[a-z].*[.,:;]$/)
+    false
+  end
+
 end
 
 def valid_phone_number?(phone)
